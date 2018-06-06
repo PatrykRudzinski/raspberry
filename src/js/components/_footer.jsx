4480307fbe0c24cc04884feb39771152bf2a45c6
Patryk <patrykrudzinski@gmail.com>
2018-06-06 12:51:50 +0200

    menu and footer syle

--- src/js/components/_footer.jsx
+++ src/js/components/_footer.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 class Footer extends React.Component {
     render() {
         return <footer className={'page-footer'}>
-            <div className={'footer__copy'}>&copy; Raspberry kingdom</div>
+            <div className={'footer__copy'}>&copy; 2014 Raspberry kingdom</div>
             <nav className={'footer__nav'}>
                 <ul className={'nav__menu'}>
-                    <li className={'menu__item'}>Cookies</li>
-                    <li className={'menu__item'}>Privacy</li>
+                    <li className={'menu__item'}><a href="#">Cookies</a></li>
+                    <li className={'menu__item'}><a href="#">Privacy</a></li>
                 </ul>
             </nav>
-            <div>Design by <span>Wizard of Oz</span></div>
+            <div className={'footer__design'}>Design by <span>Wizard of Oz</span></div>
         </footer>
     }
 }
--- src/js/components/_popup.jsx
+++ src/js/components/_popup.jsx
@@ -1,13 +1,34 @@
 import React from 'react';
 
 class Popup extends React.Component {
+    test = (e) => {
+        e.preventDefault();
+
+        const data = {
+            "login": "correct_login@example.com",
+            "password": "C0rr3Ct_P@55w0rd"
+        };
+
+        fetch('http://recruitment-api.pyt1.stg.jmr.pl/login', {
+            method: 'POST',
+            headers: {
+                'content-type' : 'application/json'
+            },
+            body: JSON.stringify(data)
+        }).then(function(response) {
+            return response.json();
+        }).then(function(data) {
+            console.log(data);
+        });
+    };
+
     render() {
         return <div className={'login-popup'}>
-            <form action="POST">
+            <form>
                 <h3 className={'popup__title'}>Are you a Raspberry Knight?</h3>
                 <input type="text" placeholder={'Email'}/>
                 <input type="password" placeholder={'Password'}/>
-                <button type='submit'>Log in</button>
+                <button type='submit' onClick={this.test}>Log in</button>
             </form>
 
         </div>
