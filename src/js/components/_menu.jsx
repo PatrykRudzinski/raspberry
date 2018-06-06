30307c9632a61c65b9a57c7f228c06bfefd4d604
Patryk <patrykrudzinski@gmail.com>
2018-06-07 01:57:00 +0200

    rwd

--- src/js/components/_menu.jsx
+++ src/js/components/_menu.jsx
@@ -2,20 +2,42 @@ import React from 'react';
 
 class Menu extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            menuOpened: false
+        }
+    }
+
+    handleBurger = () => {
+        this.setState({
+            menuOpened: !this.state.menuOpened
+        })
+    };
+
     render() {
-        return <div className={'page-topbar'}>
+        return <header className={'page-topbar'}>
                 <nav className={'nav'}>
-                    <ul className={'nav__menu'}>
-                        <li className={'menu__item'}><a href="#">About</a></li>
-                        <li className={'menu__item'}><a href="#">Offer</a></li>
-                        <li className={'menu__item'}>
-                            <a href="#"><img src="./images/raspberry.png" alt="link to home"/></a>
+                    <div className={this.state.menuOpened ?
+                        'nav__hamburger nav__hamburger--open' : 'nav__hamburger'}
+                        onClick={this.handleBurger}
+                    >
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                    <ul className={this.state.menuOpened ?
+                        'nav__menu' : 'nav__menu nav__menu--collapsed'}>
+                        <li className={'menu__item'}><a href='#'>About</a></li>
+                        <li className={'menu__item'}><a href='#'>Offer</a></li>
+                        <li className={'menu__item--icon'}>
+                           <img src='./images/raspberry.png' alt='raspberry logo'/>
                         </li>
-                        <li className={'menu__item'}><a href="#">Gallery</a></li>
-                        <li className={'menu__item'}><a href="#">Contact</a></li>
+                        <li className={'menu__item'}><a href='#'>Gallery</a></li>
+                        <li className={'menu__item'}><a href='#'>Contact</a></li>
                     </ul>
             </nav>
-        </div>
+        </header>
     };
 }
 
